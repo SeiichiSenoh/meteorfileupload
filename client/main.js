9f2fb005c57c7fa3d9a0621fb79e94dd04f11dcb
妹尾誠一 <senoh2@MacBookPro2.local>
2017-11-18 16:11:46 +0900

    入力BOXから、値を取得して、その回数分画像を保存

--- client/main.js
+++ client/main.js
@@ -3,9 +3,21 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+//コンストラクタと考えていい
+Template.upload.onCreated(function (){
+	this.count = new ReactiveVar(0);	//変数定義
+});
+
+//イベント発生時
+// click,change等のイベントの種類の書き方は、Javascript標準
+//　ただし、click buttonという書き方はmeteor固有
 Template.upload.events({
   'click button'(event, instance) {
-	Meteor.call('file.upload',5);
+	Meteor.call('file.upload',instance.count.get());
+  },
+
+  'change input'(event, instance){
+  	instance.count.set($(event.target).val());	//input boxから、入力された値を取得して、count変数に入れる
   },
 });
 
