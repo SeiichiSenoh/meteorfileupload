21d3f94c32bb22b1977128cfa7a3024d2117ac78
妹尾誠一 <senoh2@MacBookPro2.local>
2017-11-18 15:03:07 +0900

    ファイルアップロード（３回） パスは、環境変数から取得

--- server/main.js
+++ server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import '../common.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	console.log('PWD',process.env.PWD)
+  //console.log(Images);
+  for (var i = 0; i < 3; i++) {
+  	  Images.insert(process.env.PWD + '/sample.jpeg', function (err, fileObj) {
+        console.log("Updated");
+        //console.log(fileObj);
+      });
+  }
+  // Images.insert('/Users/senoh2/Desktop/meteor/gridfs1/sample.jpeg', function (err, fileObj) {
+  //       console.log("Updated");
+  //       //console.log(fileObj);
+  //     });
+
+  console.log(Images.find({}).count())
+  console.log(Images.findOne('uoJLHMKRLNinFjaDo').url());
 });
